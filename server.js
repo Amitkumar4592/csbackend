c8db34c266d16875cf4c2adfcbbf3a5ad95cb713
Amitkumar4592 <amitkumarchowdary2004@gmail.com>
2025-03-04 21:37:56 +0530

    hi

--- server.js
+++ server.js
@@ -24,4 +24,4 @@ app.use("/api/marketplace", marketplaceRoutes);
 app.use('/api/posts', blogRoutes);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`✅ Server running on port ${PORT}`));
